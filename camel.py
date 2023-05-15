7287cd14f9a401a8a5950732b61b2865eb448c2c
Leroyprod13 <126971660+leroyprod@users.noreply.github.com>
2023-05-15 15:26:08 +0700

    Update camel.py

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
-
+def change_case(str):
+    str=input('Верблюжий стиль: ' )
+    res = [str[0].lower()]
+    for c in str[1:]:
+        if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+            res.append('_')
+            res.append(c.lower())
+        else:
+            res.append(c)
+    return ''.join(res)
+print(change_case(str))
